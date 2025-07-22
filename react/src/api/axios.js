53482dc65391d40a317352b521c17e88443d0faa
IbragimKhaniev <newinterny@gmail.com>
2025-07-22 17:52:25 +0300

    Update files

--- react/src/api/axios.js
+++ react/src/api/axios.js
@@ -12,6 +12,20 @@ export const instance = axios.create({
   },
 });
 
+// Add request interceptor to include Authorization header with JWT token
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 /** Не удаляй этот код никогда */
 instance.interceptors.response.use(
   (response) => {
